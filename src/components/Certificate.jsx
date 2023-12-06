1d414617158aed600d3adb06170414f52c5114f5
Peyton Mastropolo <amyhytwn@aol.com>
2023-12-06 04:35:15 -0600

    Update Certificate.jsx

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Certificate.css';
+import certificateImage from './assets/public/images/IMG_4565.png';
 
-function Certificate({ imageUrl }) {
+function Certificate() {
   return (
     <div className="certificate-section">
       <h2>Certificate</h2>
-      <img className="certificate-image" src={} alt="Certificate" />
+      <img className="certificate-image" src={certificateImage} alt="Certificate" />
     </div>
   );
 }
 
-export default Certificate;
+export default Certificate
