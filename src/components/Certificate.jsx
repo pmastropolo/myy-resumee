7ce8fa92066c0802c397d8a246b78a32b5fc9765
Peyton Mastropolo <amyhytwn@aol.com>
2023-12-06 04:42:37 -0600

    Update Certificate.jsx

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import './Certificate.css';
-import certificateImage from './assets/public/images/IMG_4565.png';
+import certificateImage from './stc/assets/public/images/IMG_4565.png';
 
 function Certificate() {
   return (
     <div className="certificate-section">
       <h2>Google SEO Fundamentals</h2>
+
+      <div className="certificate-details">
+        <p>Completed a competitive analysis on a webpage</p>
+        <p>Interpreted brand recognition through social media</p>
+        <p>Created sitemaps and robot.txt files, plan redirects, and managed site errors</p>
+        <p>Used a variety of SEO tools to conduct an audience analysis and develop personas of ideal buyer</p>
+
+        <h3>Skills</h3>
+        <p>Social Media</p>
+        <p>Keyword Research</p>
+        <p>Marketing</p>
+        <p>Mathematical Optimization</p>
+      </div>
+
       <img className="certificate-image" src={certificateImage} alt="Certificate" />
     </div>
   );
 }
 
-export default Certificate
+export default Certificate;
