c5981630980f8fb5811d80493ea33d63f484709d
Peyton Mastropolo <amyhytwn@aol.com>
2023-12-06 04:44:06 -0600

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom'; // Import NavLink from react-router-dom
-import './Navigation.css'; // Import your CSS file
+import './Navigation.css'; 
 
 function Navigation() {
   return (
