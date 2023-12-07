03ee3aacd8df8e500f5a65de01a5d0bc171016f8
Peyton Mastropolo <amyhytwn@aol.com>
2023-12-06 18:28:39 -0600

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,19 +2,57 @@ import React from 'react';
 import './Home.css';
 
 function Home() {
+  const skills = [
+    'HTML',
+    'CSS',
+    'Git',
+    'JavaScript',
+    'Bootstrap',
+    'The DOM',
+    'APIs',
+    'jQuery',
+    'JSON',
+    'AJAX',
+    'Team Collaboration',
+    'Agile Development',
+    'Project Demonstration & Storytelling',
+    'Node.js',
+    'ES6',
+    'Object-oriented programming',
+    'Express.js',
+    'MySQL',
+    'MVC Paradigm',
+    'Sequelize',
+    'Jest',
+    'Insomnia',
+    'Progressive Web Apps',
+    'React',
+    'NoSQL',
+    'MERN Stack',
+    'Computer Science Fundamentals',
+    'GraphQL',
+    'Apollo Server',
+    'Apollo Client',
+    'React Router',
+    'React Hooks',
+    'MongoDB',
+    'SQL',
+    'RESTful APIs',
+  ];
+
   return (
+    <div className="home-container">
+      <div className="home-header">
+        <h1>Welcome to My Portfolio</h1>
+        <h2>Below, you'll find a collection of skills that represent my expertise and aspirations as a full-stack web developer.</h2>
+      </div>
 
-
-    <div className="skills-container">
-      <h1>Welcome to My Portfolio</h1>
-      <h2>Skills</h2>
       <div className="skills-list">
-        <div className="skill">
-          <p>HTML</p>
-        </div>
-        <div className="skill">
-          <p>CSS</p>
-        </div>
+        {skills.map((skill, index) => (
+          <div className="keyboard-button skill" key={index}>
+            <p>{skill}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
